52e696f62a98c86b449babb814c5aaf1f4b4856e
Velesco <velescovasile40@gmail.com>
2025-03-25 13:05:21 +0200

    update

--- frontend/src/components/layouts/DashboardLayout.js
+++ frontend/src/components/layouts/DashboardLayout.js
@@ -177,18 +177,6 @@ const DashboardLayout = ({ children }) => {
               <BarChart2 className="h-5 w-5 mr-2" />
               Rapoarte
             </Link>
-            
-            <Link
-              to="/admin/settings"
-              className={`flex items-center px-2 py-4 ${
-                isActive('/admin/settings')
-                  ? 'text-blue-600 border-b-2 border-blue-600 font-medium'
-                  : 'text-gray-500 hover:text-gray-700'
-              }`}
-            >
-              <Settings className="h-5 w-5 mr-2" />
-              Setări
-            </Link>
           </div>
         </div>
       </nav>
