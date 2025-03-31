fca22f5cc4c0349666f4f094e3200ea5b72cee6e
Velesco <velescovasile40@gmail.com>
2025-03-31 15:10:02 +0300

    updarte

--- frontend/src/components/landing/ProgramInfoSection.js
+++ frontend/src/components/landing/ProgramInfoSection.js
@@ -129,14 +129,6 @@ const ProgramInfoSection = () => {
                 <p className="text-gray-700">Tineri cu vârsta cuprinsă între <strong>18-30 ani</strong></p>
               </div>
               
-              <div className="flex items-start">
-                <div className="text-green-500 mr-3 mt-0.5 flex-shrink-0">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-                  </svg>
-                </div>
-                <p className="text-gray-700">Tineri cu vârsta cuprinsă între <strong>30-35 ani</strong></p>
-              </div>
               
               <div className="flex items-start">
                 <div className="text-green-500 mr-3 mt-0.5 flex-shrink-0">
